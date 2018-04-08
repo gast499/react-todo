c20049c6f2d6a16930127243c8b85b64ac776d8f
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-04-08 17:39:20 -0400

    Initialized the output property within the module.exports object

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,9 @@ let path = require('path');
 let LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = {
-	entry: './client/index.js'
+	entry: './client/index.js',
+	output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist')
+  }
 };
