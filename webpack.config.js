16f2f939188b882a015e4ffed57e45c0ce5e7c48
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-04-08 17:45:42 -0400

    Initialized the module property within the module.exports object

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,23 @@ module.exports = {
 	context: __dirname,
 	resolve: {
 		extensions: ['.js', '.jsx', '.json', '*']
+	},
+	module: {
+		rules: [{
+			test: /\.jsx?$/,
+			exclude: /(node_modules|bower_components)/,
+			loader: 'babel-loader',
+			options: {
+				presets: ['react', 'es2015']
+			}
+		},
+		{
+			test: /\.scss$/,
+			use: [
+				'style-loader',
+				'css-loader',
+				'sass-loader'
+			]
+      }]
 	}
 };
