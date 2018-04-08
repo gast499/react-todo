b1731ff05a96100088c5367e48a5ac058f1fbf8e
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-04-08 17:47:19 -0400

    Initialized the plugins property within the module.exports object

--- webpack.config.js
+++ webpack.config.js
@@ -28,5 +28,13 @@ module.exports = {
 				'sass-loader'
 			]
       }]
-	}
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.COSMIC_BUCKET': JSON.stringify(process.env.COSMIC_BUCKET),
+			'process.env.COSMIC_READ_KEY': JSON.stringify(process.env.COSMIC_READ_KEY),
+			'process.env.COSMIC_WRITE_KEY': JSON.stringify(process.env.COSMIC_WRITE_KEY)
+		}),
+		new LiveReloadPlugin({appendScriptTag: true})
+	]
 };
