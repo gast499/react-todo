30ef7d5b045abe50f15601797eff20915f0d4380
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2018-04-10 12:07:42 -0400

    Using node to serve static files in the index.js file

--- server/index.js
+++ server/index.js
@@ -3,3 +3,11 @@ const app = express();
 const path = require('path');
 const volleyball = require('volleyball');
 app.use(volleyball);
+
+app.use(express.static(path.resolve(__dirname, '..', 'client')));
+app.use(express.static(path.resolve(__dirname, '..', 'node_modules')));
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
